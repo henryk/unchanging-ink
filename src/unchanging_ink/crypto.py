3a79b69534e38163201d629609574eb72d70ae34
Henryk Plötz <henryk@ploetzli.ch>
2021-04-18 01:14:40 +0200

    Wip

--- src/unchanging_ink/crypto.py
+++ src/unchanging_ink/crypto.py
@@ -1,3 +1,10 @@
+from __future__ import annotations
+
+import math
+from dataclasses import dataclass, field
+from hashlib import sha512
+from typing import Iterable, List, Dict, Tuple
+
 from nacl.encoding import Base64Encoder
 from nacl.signing import SigningKey
 from sanic import Sanic
@@ -9,7 +16,51 @@ class Signer:
         self.kid = "Exp:" + self.key.verify_key.encode(Base64Encoder).decode("us-ascii")
 
     def sign(self, data: bytes) -> bytes:
-        return self.key.sign(data)
+        return self.key.sign(data).signature
+
+
+@dataclass
+class MerkleNode:
+    start: int
+    end: int
+    value: bytes
+    height: int = field(init=False)
+
+    def __post_init__(self):
+        self.height = int(math.ceil(math.log2(self.end - self.start + 1)))
+
+    @classmethod
+    def combine(cls: MerkleNode, n1: MerkleNode, n2: MerkleNode) -> MerkleNode:
+        assert n1.end + 1 == n2.start
+        return MerkleNode(
+            n1.start, n2.end, sha512(b"\x01" + n1.value + n2.value).digest()
+        )
+
+    @classmethod
+    def from_leaf(cls: MerkleNode, index: int, value: bytes) -> MerkleNode:
+        return MerkleNode(index, index, sha512(b"\x00" + value).digest())
+
+    @classmethod
+    def from_sequence(
+        cls: MerkleNode, values: Iterable[bytes]
+    ) -> Tuple[MerkleNode, Dict[Tuple[int, int], MerkleNode]]:
+        stack: List[MerkleNode] = []
+        full_index: Dict[Tuple[int, int], MerkleNode] = {}
+
+        for i, value in enumerate(values):
+            while len(stack) > 1 and stack[-2].height == stack[-1].height:
+                stack[-2] = cls.combine(stack[-2], stack[-1])
+                del stack[-1]
+                full_index[(stack[-1].start, stack[-1].end)] = stack[-1]
+            stack.append(cls.from_leaf(i, value))
+            full_index[(stack[-1].start, stack[-1].end)] = stack[-1]
+
+        while len(stack) > 1:
+            stack[-2] = cls.combine(stack[-2], stack[-1])
+            del stack[-1]
+            full_index[(stack[-1].start, stack[-1].end)] = stack[-1]
+
+        return stack[0], full_index
 
 
 def setup_crypto(app: Sanic):
--- src/unchanging_ink/worker.py
+++ src/unchanging_ink/worker.py
@@ -1,21 +1,86 @@
+from nacl.encoding import Base64Encoder
+import datetime
 import time
 import logging
 
+import sqlalchemy
+from sqlalchemy import create_engine
+
+from .crypto import MerkleNode
+from .models import signed_timestamp, interval
+from .server import db
+
 logger = logging.getLogger(__name__)
+
+
+def formulate_proof(full_index, i, row):
+    # FIXME Draw the rest of the owl
+    return {"id": row["id"]}
+
+
+def calculate_interval(conn: sqlalchemy.engine.Connection):
+    print(datetime.datetime.now().isoformat())
+    with conn.begin() as transaction:
+        s = signed_timestamp.select(
+            signed_timestamp.c.interval.is_(None), for_update=True
+        ).order_by("timestamp", "signature")
+        result = conn.execute(s)
+
+        rows = list(result)
+        if len(rows) == 0:
+            return
+
+        merkle_node, full_index = MerkleNode.from_sequence(
+            row["signature"] for row in rows
+        )
+        print("New head", merkle_node)
+
+        ith_obj = {"ith": merkle_node.value}
+        ith_b64 = Base64Encoder.encode(merkle_node.value).decode("us-ascii")
+        proofs = []
+        for i, row in enumerate(rows):
+            proofs.append(formulate_proof(full_index, i, row))
+
+        max_id = conn.execute(
+            sqlalchemy.select(
+                [
+                    sqlalchemy.func.max(
+                        interval.c.id, type_=sqlalchemy.types.String
+                    ).label("maxid")
+                ]
+            )
+        ).scalar()
+        ith_obj["id"] = 0 if max_id is None else max_id + 1
+        conn.execute(interval.insert().values(**ith_obj))
+
+        for proof in proofs:
+            id_ = proof.pop("id")
+            proof["ith"] = ith_b64
+            conn.execute(
+                signed_timestamp.update()
+                .where(signed_timestamp.c.id == id_)
+                .values(interval=ith_obj["id"], proof=proof)
+            )
 
 
 def main():
     logger.info("Upgrading database")
     import alembic.config
+
     alembicArgs = [
-        '--raiseerr',
-        'upgrade', 'head',
+        "--raiseerr",
+        "upgrade",
+        "head",
     ]
     alembic.config.main(argv=alembicArgs)
+
+    engine = create_engine(str(db.url))
 
     logger.info("Worker ready")
     while True:
         time.sleep(1)
+        with engine.connect() as conn:
+            calculate_interval(conn)
 
 
 if __name__ == "__main__":
