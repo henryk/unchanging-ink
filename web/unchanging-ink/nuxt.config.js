5294e9c1c8f8edf9e3114e002dc21815d5f9d43e
Henryk Plötz <henryk@ploetzli.ch>
2021-05-09 17:21:26 +0200

    Add webpack report as artifact

--- web/unchanging-ink/nuxt.config.js
+++ web/unchanging-ink/nuxt.config.js
@@ -65,7 +65,11 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    analyze: {
+      analyzerMode: 'static',
+    },
+  },
 
   modern: 'client',
 
