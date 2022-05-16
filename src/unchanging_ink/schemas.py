1792ece16d2ef9512979e09a2ad3161267dc9c9f
Henryk Plötz <henryk@ploetzli.ch>
2022-05-16 03:25:01 +0200

    Add schemas

--- src/unchanging_ink/schemas.py
+++ src/unchanging_ink/schemas.py
@@ -1,4 +1,58 @@
-from dataclasses import dataclass
+import datetime
+from dataclasses import dataclass, asdict
+from typing import Optional
+
+import cbor2
+import orjson
+
+
+class CBORMixin:
+    @classmethod
+    def from_cbor(cls, data: bytes):
+        return cls(**cbor2.loads(data))
+
+    def to_cbor(self) -> bytes:
+        return cbor2.dumps(asdict(self), canonical=True)
+
+
+class JSONMixin:
+    @classmethod
+    def from_json(cls, data: bytes):
+        return cls(**orjson.loads(data))
+
+    def to_json(self) -> bytes:
+        return orjson.dumps(asdict(self))
+
+
+@dataclass
+class TimestampRequest(CBORMixin, JSONMixin):
+    data: str
+    options: list[str]
+
+
+@dataclass
+class TimestampStructure(CBORMixin):
+    data: str
+    timestamp: datetime.datetime
+    typ: str = "ts"
+    version: str = "1"
+
+
+@dataclass
+class IntervalProofStructure:
+    mth: str
+    itmh: bytes
+    a: int
+    path: list[bytes]
+
+
+@dataclass
+class Timestamp(CBORMixin, JSONMixin):
+    hash: bytes
+    timestamp: datetime.datetime
+    typ: str = "ts"
+    version: str = "1"
+    proof: Optional[IntervalProofStructure] = None
 
 
 @dataclass
