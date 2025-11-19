b9d605a205e8ae13897f6cf81e32b0ed106e0128
florian <60937022+cdr-chakotay@users.noreply.github.com>
2025-11-19 09:40:09 +0100

    feat: Enhance article loading state and implement ContentRenderer for dynamic content

--- web/unchanging-ink/pages/doc/index.vue
+++ web/unchanging-ink/pages/doc/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <article>
-    <ContentDoc path="/doc/index" />
-  </article>
+  <div>
+    <div v-if="!article">Loading or no article found...</div>
+    <article v-else>
+      <ContentRenderer :value="article" />
+    </article>
+  </div>
 </template>
+
+<script setup lang="ts">
+const { data: article, error } = await useAsyncData('doc-index', () =>
+  queryContent('/doc/index').findOne(),
+)
+
+if (process.client) {
+  console.log('Doc index article:', article.value)
+  console.log('Doc index error:', error.value)
+}
+</script>
